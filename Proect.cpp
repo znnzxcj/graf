804cd38e5af5074ce1971034a24165a3bad222a1
Nikita <zmc55@mail.ru>
2022-07-05 23:12:02 +0300

    Add  addElem

--- Proect.cpp
+++ Proect.cpp
@@ -1,20 +1,28 @@
-﻿// Proect.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+#include<iostream>
+#include<vector>
 
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
-}
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+using namespace std;
+struct spis {
+	vector<int> ver;
+	struct spis* next;
+};
+struct size_head {
+	size_t size;
+	struct spis* head;
+};
+void addElem(size_head*& list, vector<int> matr, int n) {
+	spis* newElem = new spis;
+	for (int i = 0; i < n; i++) {
+		newElem->ver.push_back(matr[i]);
+	}
+	if (list->size == 0) {
+		list->head = newElem;
+		list->head->next = list->head;
+	}
+	else {
+		struct spis* temp;
+		temp = list->head;
+		list->head = newElem;
+		newElem->next = temp;
+	}
+	++list->size;
